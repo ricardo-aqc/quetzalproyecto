bbee6c26b6c2a14e6b97a720fc73b9ea02a20fe0
juanpyg <93606125+juanpyg@users.noreply.github.com>
2023-11-09 21:48:24 -0600

    Add files via upload

---
+++ constructora.sql
@@ -0,0 +1,124 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 10-11-2023 a las 04:39:28
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `constructora`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `imagen`
+--
+
+CREATE TABLE `imagen` (
+  `imagen_id` int(11) NOT NULL,
+  `obra_id` int(11) NOT NULL,
+  `imagen` blob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `obra`
+--
+
+CREATE TABLE `obra` (
+  `order_id` int(11) NOT NULL,
+  `descripcion` varchar(200) NOT NULL,
+  `url_ubicacion` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `obra_imagen`
+--
+
+CREATE TABLE `obra_imagen` (
+  `obra_imagen_id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL,
+  `imagen_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `usuario` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `imagen`
+--
+ALTER TABLE `imagen`
+  ADD PRIMARY KEY (`imagen_id`),
+  ADD KEY `obra_id` (`obra_id`);
+
+--
+-- Indices de la tabla `obra`
+--
+ALTER TABLE `obra`
+  ADD PRIMARY KEY (`order_id`);
+
+--
+-- Indices de la tabla `obra_imagen`
+--
+ALTER TABLE `obra_imagen`
+  ADD PRIMARY KEY (`obra_imagen_id`),
+  ADD KEY `order_id` (`order_id`),
+  ADD KEY `imagen_id` (`imagen_id`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`usuario`);
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `imagen`
+--
+ALTER TABLE `imagen`
+  ADD CONSTRAINT `imagen_ibfk_1` FOREIGN KEY (`obra_id`) REFERENCES `obra` (`order_id`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `obra_imagen`
+--
+ALTER TABLE `obra_imagen`
+  ADD CONSTRAINT `obra_imagen_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `obra` (`order_id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `obra_imagen_ibfk_2` FOREIGN KEY (`imagen_id`) REFERENCES `imagen` (`imagen_id`) ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
